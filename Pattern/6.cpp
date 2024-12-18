50659e24d28c968aac6fd6fe494a1b6e1ec935bf
Ravindra Kachariya <ravindrakachariya@gmail.com>
2024-12-18 18:49:09 +0530

    Add | Pattern

---
+++ Pattern/6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/*
+A 
+B B 
+C C C 
+D D D D 
+E E E E E 
+*/
+
+int main()
+{
+    int i, j, n;
+    char ch = 'A';
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        ch++;
+        cout << endl;
+    }
+
+    return 0;
+}
