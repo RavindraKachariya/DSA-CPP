e6c51dc82ad0f11312ec44fb1d21596e9bde93ea
ravindra kachariya <ravindrakachariya@gmail.com>
2025-01-11 12:59:34 +0530

    Add | 2D Array

---
+++ Array in Detail/2D Array/2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int ans = 0;
+
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
