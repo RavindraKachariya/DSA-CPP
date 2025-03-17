823108041296b7a744950510a8b78eb96a232576
Ravindra Kachariya <ravindrakachariya@gmail.com>
2025-03-17 10:12:54 +0530

    Add | Stack Exam

---
+++ Stack Exam/Valid-Parentheses.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> stk;
+    for (char c : s)
+    {
+        if (c == '(' || c == '{' || c == '[')
+        {
+            stk.push(c);
+        }
+        else
+        {
+            if (stk.empty())
+            {
+                return false;
+            }
+            char top = stk.top();
+            if ((c == ')' && top == '(') ||
+                (c == '}' && top == '{') ||
+                (c == ']' && top == '['))
+            {
+                stk.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return stk.empty();
+}
+
+int main()
+{
+    string s;
+    cout << "Enter a string of parentheses: ";
+    cin >> s;
+
+    if (isValid(s))
+    {
+        cout << "Valid Parentheses" << endl;
+    }
+    else
+    {
+        cout << "Invalid Parentheses" << endl;
+    }
+    
+    return 0;
+}
